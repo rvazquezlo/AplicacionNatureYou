7c13b3234b1d71e5775fbefe9390665acf0a082e
rvazquezlo <regis.vazquez@outlook.com>
2018-12-01 13:11:27 -0600

    Initial commit

---
+++ app/src/main/java/com/example/carlos/aplicacionnatureyou/AprobarProductos.java
@@ -0,0 +1,13 @@
+package com.example.carlos.aplicacionnatureyou;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AprobarProductos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aprobar_productos);
+    }
+}

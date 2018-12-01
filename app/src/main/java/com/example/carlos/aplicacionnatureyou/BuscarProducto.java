d6207489491bc9d1a3a00599ce0467c4abf0448d
rvazquezlo <regis.vazquez@outlook.com>
2018-12-01 13:19:42 -0600

    Crear ventana BuscarProducto

---
+++ app/src/main/java/com/example/carlos/aplicacionnatureyou/BuscarProducto.java
@@ -0,0 +1,13 @@
+package com.example.carlos.aplicacionnatureyou;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BuscarProducto extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_buscar_producto);
+    }
+}
